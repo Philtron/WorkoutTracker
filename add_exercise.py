9d93aa9b7ae4d3564ff51372e249d526b3bda34c
Philip Sauer <psauer0@gmail.com>
2023-03-01 16:25:18 -0700

    Working on add exercise

--- add_exercise.py
+++ add_exercise.py
@@ -8,32 +8,72 @@ class AddExercise(tk.Toplevel):
         super().__init__(master)
 
         # val = (workout_id, exercise_id, lifter_id, weight, reps, set_number, notes)
+
         self.mydb = db_connection
         self.my_cursor = self.mydb.cursor()
 
         self.title("Add Exercise Log")
-        self.geometry("400x400")
+        self.geometry("300x300")
+        self.grid_frame = tk.Frame(self)
+        self.grid_frame.pack(fill=tk.BOTH, expand=True)
 
-        self.exercise_label = tk.Label(text="Exercises")
-        self.exercise_options = tk.StringVar(self)
+        self.exercise_label = tk.Label(self.grid_frame, text="Exercises")
+        self.exercise_options = tk.StringVar(self.grid_frame)
         self.exercise_options.set("")
-        self.exercise_option_menu = tk.OptionMenu(self, self.exercise_options, *self.get_exercises())
-        self.exercise_option_menu.pack()
+        self.exercise_option_menu = tk.OptionMenu(self.grid_frame, self.exercise_options, *self.get_exercises())
 
-        self.lifter_label = tk.Label(text="Lifters")
-        self.lifter_options = tk.StringVar(self)
+        self.lifter_label = tk.Label(self.grid_frame, text="Lifters")
+        self.lifter_options = tk.StringVar(self.grid_frame)
         self.lifter_options.set("")
-        self.lifter_option_menu = tk.OptionMenu(self, self.lifter_options, *self.get_lifters())
-        self.lifter_option_menu.pack()
+        self.lifter_option_menu = tk.OptionMenu(self.grid_frame, self.lifter_options, *self.get_lifters())
 
-        self.workout_label = tk.Label(text="Workouts")
-        self.workout_options = tk.StringVar(self)
+        self.workout_label = tk.Label(self.grid_frame, text="Workouts")
+        self.workout_options = tk.StringVar(self.grid_frame)
         self.workout_options.set("")
-        self.workout_option_menu = tk.OptionMenu(self, self.workout_options, *self.get_workouts())
-        self.workout_option_menu.pack()
+        self.workout_option_menu = tk.OptionMenu(self.grid_frame, self.workout_options, *self.get_workouts())
 
-        self.back_button = tk.Button(self, text="Back", command=self.go_back)
-        self.back_button.pack()
+        self.back_button = tk.Button(self.grid_frame, text="Back", command=self.go_back)
+
+        self.exercise_label.grid(row=0, column=0, sticky=tk.W)
+        self.exercise_option_menu.grid(row=0, column=1, sticky=tk.W+tk.E)
+        self.lifter_label.grid(row=1, column=0, sticky=tk.W)
+        self.lifter_option_menu.grid(row=1, column=1, sticky=tk.W+tk.E)
+        self.workout_label.grid(row=2, column=0, sticky=tk.W)
+        self.workout_option_menu.grid(row=2, column=1, sticky=tk.W+tk.E)
+        self.back_button.grid(row=3, column=0, columnspan=2)
+
+        self.grid_frame.columnconfigure(0, weight=1)
+        self.grid_frame.columnconfigure(1, weight=1)
+        self.grid_frame.rowconfigure(0, weight=1)
+        self.grid_frame.rowconfigure(1, weight=1)
+        self.grid_frame.rowconfigure(2, weight=1)
+        self.grid_frame.rowconfigure(3, weight=1)
+        # self.exercise_label = tk.Label(self, text="Exercises")
+        # self.exercise_options = tk.StringVar(self)
+        # self.exercise_options.set("")
+        # self.exercise_option_menu = tk.OptionMenu(self, self.exercise_options, *self.get_exercises())
+        #
+        # self.lifter_label = tk.Label(self, text="Lifters")
+        # self.lifter_options = tk.StringVar(self)
+        # self.lifter_options.set("")
+        # self.lifter_option_menu = tk.OptionMenu(self, self.lifter_options, *self.get_lifters())
+        #
+        # self.workout_label = tk.Label(self, text="Workouts")
+        # self.workout_options = tk.StringVar(self)
+        # self.workout_options.set("")
+        # self.workout_option_menu = tk.OptionMenu(self, self.workout_options, *self.get_workouts())
+        #
+        # self.back_button = tk.Button(self, text="Back", command=self.go_back)
+        #
+        # self.workout_label.grid(row=0, column=0,sticky=tk.W)
+        # self.workout_option_menu.grid(row=0, column=1, columnspan=2, sticky=tk.W+tk.E)
+        # self.lifter_label.grid(row=1, column=0, sticky=tk.W)
+        # self.lifter_option_menu.grid(row=1, column=1, columnspan=2, sticky=tk.W+tk.E)
+        # self.exercise_label.grid(row=2, column=0, sticky=tk.W)
+        # self.exercise_option_menu.grid(row=2, column=1, columnspan=2, sticky=tk.W+tk.E)
+        # self.back_button.grid(row=3, column=0, columnspan=2, sticky=tk.W+tk.E)
+
+
 
     def get_exercises(self):
         # Get the list of exercises from the database
